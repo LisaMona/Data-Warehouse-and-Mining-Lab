18ac8438b653837628ea294df52c26f0e6f25933
Mona Lisa <mnls4u@gmail.com>
2018-09-04 15:40:09 +0530

    Add files via upload

---
+++ 04.09.18/decisiontree.py
@@ -0,0 +1,154 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn 
+
+
+
+#read the dataset
+df = pd.read_csv("car.csv")
+
+#assign name to the columns
+df.columns = ['buying','maint','doors','persons','lug_boot','safety','classes']
+
+#splitting into dependent and independent variables
+X = df.iloc[:,:-1]
+y = df.iloc[:,-1]
+
+#Categorical values
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+X = X.apply(LabelEncoder().fit_transform)
+onehotencoder = OneHotEncoder(categorical_features=[2,3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+#fitting the decision tree model
+from sklearn import tree,metrics
+dtree = tree.DecisionTreeClassifier(criterion = 'gini',splitter = 'best',random_state=0)
+dtree.fit(X_train, y_train)
+
+#predicting the results
+y_pred = dtree.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+count_misclassified = (y_test != y_pred).sum()
+accuracy = metrics.accuracy_score(y_test, y_pred)
+
+print('Misclassified samples: {}'.format(count_misclassified))
+print("Confusion Matrix: ",cm)
+print('Accuracy: {:.2f}'.format(accuracy))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classification (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree Classification (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+data = pd.read_csv('car.csv',names=['buying','maint','doors','persons','lug_boot','safety','class'])
+data['class'],class_names = pd.factorize(data['class'])
+data['buying'],_ = pd.factorize(data['buying'])
+data['maint'],_ = pd.factorize(data['maint'])
+data['doors'],_ = pd.factorize(data['doors'])
+data['persons'],_ = pd.factorize(data['persons'])
+data['lug_boot'],_ = pd.factorize(data['lug_boot'])
+data['safety'],_ = pd.factorize(data['safety'])
+'''
+
